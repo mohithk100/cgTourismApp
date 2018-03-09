3afc4d833e66cffcf84f7e23d49355f204c20b10
mohithk100 <mohithkhatri100@gmail.com>
2018-03-09 18:37:28 +0530

    Added Registeration confirmation email(sendgrid)

--- userAccounts/api/serializers.py
+++ userAccounts/api/serializers.py
@@ -5,6 +5,10 @@ from django_countries.serializers import CountryFieldMixin
 from .validators import PhoneNumberValidator
 from twilio.rest import Client
 import random
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+
 
 
 
@@ -33,22 +37,27 @@ class UserSerializer(CountryFieldMixin,serializers.ModelSerializer):
         except:
             otp = ''
         if otp=='':
-            auth_token = '1729bdc3c04616dbc8ca2a1fde8b5a46'
-            account_sid = 'ACf5adb3a8c7f5bfbffb575a2837e707f4'
-            otp = random.randint(100000,999999)
-            request.session['otp']=otp
-            twilio_number = "+16304746450"
-            to_number = str(validated_data['mobileNumber'])
-            body = "Welcome To Chhattisgarh Tourism App. Your verification code is :" + str(otp)
-            client = Client(account_sid , auth_token)
-            message = client.api.messages.create(to_number,from_=twilio_number,body = body)
-            if message.sid: 
-                validated_data['success']=True
-                validated_data['message']='OTP sent successfully'
-            else:
-                validated_data['success']=False
-                validated_data['message']='OTP delivery failed'
-            return validated_data
+            try:
+                auth_token = '1729bdc3c04616dbc8ca2a1fde8b5a46'
+                account_sid = 'ACf5adb3a8c7f5bfbffb575a2837e707f4'
+                otp = random.randint(100000,999999)
+                request.session['otp']=otp
+                twilio_number = "+16304746450"
+                to_number = str(validated_data['mobileNumber'])
+                body = "Welcome To Chhattisgarh Tourism App. Your verification code is :" + str(otp)
+                client = Client(account_sid , auth_token)
+                message = client.api.messages.create(to_number,from_=twilio_number,body = body)
+                if message.sid: 
+                    validated_data['success']=True
+                    validated_data['message']='OTP sent successfully'
+                else:
+                    validated_data['success']=False
+                    validated_data['message']='OTP delivery failed'
+                return validated_data
+            except:
+                validated_data['message'] = 'The Number is not a verified twilio number.'
+                validated_data['success'] = False
+                return validated_data
         else:
             if int(otp) == request.session['otp']:
                 user = User(
@@ -58,6 +67,16 @@ class UserSerializer(CountryFieldMixin,serializers.ModelSerializer):
                 user.set_password(validated_data.pop('password'))
                 if ('email' in list_of_keys):
                     user.email = validated_data.pop('email')
+                    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+                    from_email = Email("mohithkhatri100@gmail.com")
+                    to_email = Email(user.email)
+                    subject = "Welcome to Chhattisgarh Tourism"
+                    content = Content("text/plain", "Congratulatins! on successfully registering with the Chhattisgarh tourism mobile application.May of have a great time in a state full of surprises.\n\nRegards,\n\nChhattisgarh Tourism Department.")
+                    mail = Mail(from_email, subject, to_email, content)
+                    response = sg.client.mail.send.post(request_body=mail.get())
+                    print(response.status_code)
+                    print(response.body)
+                    print(response.headers)
                 if ('first_name' in list_of_keys):
                     user.first_name = validated_data.pop('first_name')
                 if ('last_name' in list_of_keys):
